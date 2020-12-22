309ad18570a6593759f4a5ec98169c6f1048c2b3
PatrickYoo <dkajffkeh@gmail.com>
2020-12-22 23:18:39 +0900

    ..

--- testrepo/src/testrepo/Test2.java
+++ testrepo/src/testrepo/Test2.java
@@ -2,4 +2,23 @@ package testrepo;
 
 public class Test2 {
 
+	public void Fuck() {
+		
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		System.out.println("이게 지워져야함");
+		
+		
+	}
+	
+	
 }
